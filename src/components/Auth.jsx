42865348000c1946d3076bbbb70c6dd7c6af4fbe
I-Notna-I <a.a.ivanichenko@inbox.ru>
2022-12-17 09:02:25 +0200

    Return Function Rework; Form Receivement Rework

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,112 +1,111 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef  } from "react";
 import { useNavigate } from "react-router-dom";
 
+import RegisMenu from "./menus/RegisMenu";
+import LoginMenu from "./menus/LoginMenu";
 
-export default function () {
-  let [authMode, setAuthMode] = useState("signin")
+function Auth() {
+  
+  // (?) Separate menus for Login and Registration
+  let loginMenu = <LoginMenu swapFunction={switchAuthMode} submitFunction={submitForm}/>;
+  let regisMenu = <RegisMenu swapFunction={switchAuthMode} submitFunction={submitForm}/>;
 
-  const [username, setusername] = useState("");
-  const [password, setpassword] = useState("");
-  const [authenticated, setauthenticated] = useState(localStorage.getItem("authenticated") || false);
+  // (?) Current mode and Current menu
+  let [authMode, setAuthMode] = useState("LOGIN");
+  let [authMenu, setAuthMenu] = useState(loginMenu);
 
-  const users = [{ username: "1", password: "1234" }, { username: "2", password: "1" }]; //DB analog for tests :)
+  // [!] TEMP ----------------------------------------------------------------
+  let [isAuthenticated, setAuthenticated]= useState(localStorage.getItem("authenticated") || false);
+    // [!] TODO: Check why it doesn't work properly...
+    // let isAuthenticated = useRef( localStorage.getItem("isAuthenticated")===true ? true : false);
+  // DB analog for tests :)
+  const users = [{ username: "1", password: "1234" }, { username: " ", password: " " }, { username: "admin", password: "admin" }, { username: "ABOBA", password: "1" }]; 
+  // -------------------------------------------------------------------------
 
+  // Navigation for redirection to Main Menu
+  const navigate = useNavigate(); 
 
-  const navigate = useNavigate(); //navigation for redirect on login
+  function submitForm(formInput)
+  {
+    // (!!!) Curring Pattern
+    // TODO: read more about this pattern...
+    return(
+      (event) =>
+      {
+        console.log("User authenticated? ",isAuthenticated);
+        event.preventDefault();
+        console.log("Submition...");
+        console.log(formInput);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    const account = users.find((user) => user.username === username);
-    if (localStorage.getItem("authenticated")) {
-      console.log('already logged in');
-    }
-    else if (account && account.password === password) {
-      setauthenticated(true)
-      localStorage.setItem("authenticated", true);
-      console.log('authenticated');
-      navigate("/");
-    }
-  };
-
-  const changeAuthMode = () => {
-    setAuthMode(authMode === "signin" ? "signup" : "signin")
+        // [!] TODO: MASSIVE REWORK
+        switch(authMode)
+        {
+          case "REGIS":
+            console.log("Registration...");
+            break;
+          case "LOGIN":
+            login(formInput);
+            break; 
+          default:
+        }
+      }
+    );
   }
 
-  if (authMode === "signin") {
-    return (
-      <div className="Auth-form-container">
-        <form onSubmit={handleSubmit} className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign in</h3>
-            <div className="form-group mt-3">
-              <input
-                type="name"
-                className="form-control mt-1"
-                placeholder="Username"
-                onChange={(e) => setusername(e.target.value)}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Password"
-                onChange={(e) => setpassword(e.target.value)}
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-              <div className="text-center">
-                Not registered yet?{" "}
-                <span className="link-primary" onClick={changeAuthMode}>
-                  Sign Up
-                </span>
-              </div>
-            </div>
 
-          </div>
-        </form>
-      </div>
-    )
+  function login(formInput)
+  {
+    console.log("Searcing user...");
+    for (let i = 0; i < users.length; i++) 
+    {
+      if ((formInput["password"]===users[i].password) && (formInput["username"]===users[i].username))
+      {
+        console.log("User found...");
+        console.log("User authenticated? ",isAuthenticated);
+        if(isAuthenticated)
+        {
+          console.log('User SHOULD BE ALREADY logged in...');
+          localStorage.setItem("isAuthenticated",true)
+          navigate("/");
+        }
+        else
+        {
+          setAuthenticated(true);
+          localStorage.setItem("isAuthenticated",true)
+          console.log("User authenticated!");
+          navigate("/");
+        }
+        
+        return;
+      }
+    } 
+    console.log("User not found...");
   }
 
-  return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign up</h3>
 
-          <div className="form-group mt-3">
-            <input
-              type="name"
-              className="form-control mt-1"
-              placeholder="Your unique username"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Your strong password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-            <div className="text-center">
-              Already registered?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign In
-              </span>
-            </div>
-          </div>
+  function switchAuthMode()
+  {
+    console.log("Switching Auth Mode...");
+    setAuthMode(authMode === "REGIS" ? "LOGIN" : "REGIS");
 
-        </div>
-      </form>
+    // [!] ОЧЕНЬ тупой способ (но рабочий)
+      // if(authMode === "REGIS") {authMode="LOGIN";} else {authMode="REGIS";}
+      // setAuthMode(authMode);
+      // if(authMode === "REGIS") {authMenu=<RegisMenu/>;} else {authMenu=<LoginMenu/>;}
+      // setAuthMenu(authMenu);
+
+    // [!] Если сделать изменение таким путем, то authMenu не изменится сразу
+      // setAuthMode(authMode === "REGIS" ? "LOGIN" : "REGIS");
+      // setAuthMenu(authMode === "REGIS" ? <RegisMenu/> : <LoginMenu/>);
+  }
+
+  useEffect(() => {setAuthMenu( authMode === "REGIS" ? regisMenu : loginMenu)}, [authMode]);
+
+  return(
+    <div className="Wrapper-narrow">
+      {authMenu}
     </div>
   )
+
 }
+export default Auth;
