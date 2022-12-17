3998c3f5680137edc414de72636621296488688a
I-Notna-I <a.a.ivanichenko@inbox.ru>
2022-12-17 08:53:55 +0200

    Added Button Component into Links

---
+++ src/components/Main.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState} from "react";
+import { Navigate, Link } from "react-router-dom";
+
+import SimpleButton from "./ui_components/SimpleButton";
+
+function Main() {
+
+    // [!] TEMP
+    if (!localStorage.getItem("isAuthenticated")) {
+        console.log("not authenticated");
+        console.log(localStorage.getItem("isAuthenticated"));
+
+        return <Navigate replace to="/auth" />;
+    } else {
+        return (
+            <div className="Wrapper-main">
+                <div className="Main-box">
+                    <h1 className="Title">Main Menu</h1>
+
+                    <div className="Main-buttons">
+                        <Link to='/seacrh'>
+                            <SimpleButton className="Button"  text="Find a lobby"/>
+                        </Link>
+
+                        <Link to='/create'>
+                            <SimpleButton className="Button" text="Create a lobby"/>
+                        </Link>
+
+                        <Link to='/profile'>
+                            <SimpleButton className="Button" text="Profile"/>
+                        </Link>
+
+                        <Link to='/about'>
+                            <SimpleButton className="Button"  text="About"/>
+                        </Link>
+
+                        <Link to='/settings'>
+                            <SimpleButton className="Button" text="Settings"/>
+                        </Link>
+                    </div>
+                </div >
+            </div>
+            
+        );
+    }
+};
+
+
+export default Main
