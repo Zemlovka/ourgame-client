8c87a6d41dc7c8aa7298bc7823a30a8781f80e5b
Nikita Korotov <korn03@vse.cz>
2023-01-19 04:13:55 +0100

    Added useState and useEffect for txt in search

--- src/components/ui_components/LobbyListItem.jsx
+++ src/components/ui_components/LobbyListItem.jsx
@@ -3,9 +3,10 @@ import SimpleButton from "./SimpleButton";
 // 🔑🙂☹️
 
 function LobbyListItem(lobby) {
+    
 
     return(
-        <div className="Lobby-item" id={lobby.id}>
+        <div className={"Lobby-item"} id={lobby.id}>
             <div className="Title">{lobby.name}</div>
             <div className="Player-count">{lobby.playersCount}/{lobby.maxPlayers} 🙂</div>
             <div className="With-password">{lobby.private? "🔒" : "🔓"}</div>
